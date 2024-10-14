f31b68cbeb27c0998b77ce5f75ff4c2d865b09ba
Abdllauhramdan <abdram2003@gmail.com>
2024-10-14 17:40:21 +0300

    creating the fundimantal fils for surgeries,records and appintmant

--- Modules/Records/app/Models/MedicalRecord.php
+++ Modules/Records/app/Models/MedicalRecord.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Modules\Records\Database\Factories\MedicalRecordFactory;
 
-class Medical_Record extends Model
+class MedicalRecord extends Model
 {
     use HasFactory;
 
--- Modules/Users/app/Http/Requests/StoreDoctorRequest.php
+++ Modules/Users/app/Http/Requests/StoreDoctorRequest.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace Modules\Users\Http\Requests;
 
 use App\Services\ApiResponseService;
